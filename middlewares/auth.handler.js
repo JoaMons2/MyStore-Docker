26d2f43c45c6e5e55eae319ee85293f4b3934c38
Joanna Monserrat Espinoza Sánchez <ejoannamonserrat@gmail.com>
2023-08-01 22:25:26 -0600

    Control de roles

--- middlewares/auth.handler.js
+++ middlewares/auth.handler.js
@@ -2,7 +2,6 @@ const boom = require('@hapi/boom');
 
 const { config } = require('./../config/config');
 
-
 function checkApiKey(req, res, next) {
   const apiKey = req.headers['api'];
   if (apiKey === config.apiKey) {
@@ -12,4 +11,26 @@ function checkApiKey(req, res, next) {
   }
 }
 
-module.exports = { checkApiKey }
+function checkAdminRole(req, res, next) {
+  const user = req.user;
+  if (user.role === 'admin') {
+    next();
+  } else {
+    next(boom.unauthorized());
+  }
+}
+
+
+function checkRoles(...roles) {
+  return (req, res, next) => {
+    const user = req.user;
+    if (roles.includes(user.role)) {
+      next();
+    } else {
+    next(boom.unauthorized());
+    }
+  }
+}
+
+
+module.exports = { checkApiKey, checkAdminRole, checkRoles }
